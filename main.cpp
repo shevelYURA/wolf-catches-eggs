15dc07f105015e30bf5dde1626046f12d252dff7
ShevelYURA <promacterto@gmail.com>
2026-03-04 17:15:17 +0300

    Смена курса в связи с неодобрением трудности предыдущего варианта А.В. Теперь яйца падают сверху. Уменьшен масштаб как игрока так и яиц, увеличено окно до 1920 1080. Игрок (волк и корзина) вынесен в отдельный класс, как было с яйцами. Игрок теперь передвигается не по заданным позициям, а сам. Другие изменения концептуально ничего не меняют (вроде бы).

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include "Eggs.h"
 
 using namespace sf;
 
 int main()
 {
-    RenderWindow window(VideoMode({ 1280, 720 }), "Wolf Catches Eggs");
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    //окно
+    RenderWindow window(VideoMode({ 1920, 1080 }), "Wolf Catches Eggs");
     
     Image icon;
     if (!icon.loadFromFile("image/icon.png")) {
@@ -12,35 +16,48 @@ int main()
     }
     window.setIcon(icon);
 
-    //"ВОЛК"
+    //------------------------------------------------------------------------------------------//
+    
+    //Объекты:
+    
+    //Волк
     Texture texWolf;
     if (!texWolf.loadFromFile("image/wolfTexture.png")) {
         return 2;
     }
-    RectangleShape WOLF(Vector2f(100, 300));
+    RectangleShape WOLF(Vector2f(50, 150));
     WOLF.setTexture(&texWolf);
-    WOLF.setPosition(Vector2f(590, 320));
+    WOLF.setPosition(Vector2f(910, 1000));
 
     //Корзинка
     Texture texBasket;
     if (!texBasket.loadFromFile("image/basketTexture.png")) {
         return 3;
     }
-    RectangleShape Basket(Vector2f(150, 50));
+    RectangleShape Basket(Vector2f(50, 15));
     Basket.setTexture(&texBasket);
-    Basket.setPosition(Vector2f(565, 400));
+    Basket.setPosition(Vector2f(WOLF.getPosition().x, WOLF.getPosition().y - 15*9));
 
     //Яйца
-    Texture texEggs;
-    if (!texEggs.loadFromFile("image/eggsTexture.jpg")) {
-        return 3;
-    }
-    CircleShape Eggs(25.0f);
-    Eggs.setTexture(&texEggs);
-    Eggs.setPosition(Vector2f(0, 0));
+    Eggs eggs[10];
+    int count_eggs = 10;
+
+    //-----------------------------------------------------------------------------------------//
+
+    //переменные
+    bool GameOver = false;
+    Clock clock;
+    float time, timeEggs;
+
+    const float WOLF_SPEED = 0.5f;
 
     while (window.isOpen())
     {
+        time = clock.getElapsedTime().asMicroseconds();
+        timeEggs = time / 3000;
+        time = time / 6000;
+        clock.restart();
+
         while (const std::optional event = window.pollEvent())
         {
             if (event->is <Event::Closed>())
@@ -50,11 +67,11 @@ int main()
                 const auto& keyEvent = event->getIf<Event::KeyPressed>();
                 if (keyEvent->code == Keyboard::Key::A) {
                     WOLF.setPosition(Vector2f(300, 300));
-                    Basket.setPosition(Vector2f(210, Basket.getPosition().y));
+                    Basket.setPosition(Vector2f(WOLF.getPosition().x - 110, Basket.getPosition().y));
                 }
                 if (keyEvent->code == Keyboard::Key::D) {
                     WOLF.setPosition(Vector2f(820, 300));
-                    Basket.setPosition(Vector2f(910, Basket.getPosition().y));
+                    Basket.setPosition(Vector2f(WOLF.getPosition().x + 90, Basket.getPosition().y));
                 }
                 if (keyEvent->code == Keyboard::Key::W) {
                     Basket.setPosition(Vector2f(Basket.getPosition().x, 300));
@@ -64,10 +81,19 @@ int main()
                 }
             }
         }
+
+        for (int i = 0; i < count_eggs; i++)
+        {
+            eggs[i].move(timeEggs);
+
+            if (eggs[i].collision(Basket.getGlobalBounds())) {
+                eggs[i].restart();
+            }
+        }
         window.clear();
         window.draw(WOLF);
         window.draw(Basket);
-        window.draw(Eggs);
+        for (int i = 0; i < count_eggs; i++) eggs[i].draw(window);
         window.display();
     }
 }
