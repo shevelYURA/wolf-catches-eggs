80dd2c9e3a5b345e1013c6eebbb19f32f8fdce1f
alina05dagestan <kseniaalieva171@gmail.com>
2026-04-10 18:54:30 +0300

    Update Eggs.h
    смена цвета яиц

--- Eggs.h
+++ Eggs.h
@@ -3,13 +3,18 @@
 
 class Egg : public FallingObject {
 private:
-    bool isGolden;  //золотое яйцо или нет
+    bool isGolden;  // золотое яйцо или нет
 
 public:
     Egg() : FallingObject("image/eggsTexture.png", Vector2f(40, 35)), isGolden(false) {}
     
     void setGolden(bool golden) {
         isGolden = golden;
+        if (isGolden) {
+            setColor(sf::Color(255, 215, 0));  // Золотой цвет
+        } else {
+            setColor(sf::Color::White);        // Белый цвет
+        }
     }
     
     bool getGolden() const {
