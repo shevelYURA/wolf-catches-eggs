e5ebf4227c76ea073296b4f4da560253b9f15fef
alina05dagestan <kseniaalieva171@gmail.com>
2026-04-10 18:40:10 +0300

    Update Eggs.h
    Добавила флаг для золотых яиц

--- Eggs.h
+++ Eggs.h
@@ -2,6 +2,17 @@
 #include "FallingObject.h"
 
 class Egg : public FallingObject {
+private:
+    bool isGolden;  //золотое яйцо или нет
+
 public:
-    Egg() : FallingObject("image/eggsTexture.png", Vector2f(40, 35)) {}
+    Egg() : FallingObject("image/eggsTexture.png", Vector2f(40, 35)), isGolden(false) {}
+    
+    void setGolden(bool golden) {
+        isGolden = golden;
+    }
+    
+    bool getGolden() const {
+        return isGolden;
+    }
 };
