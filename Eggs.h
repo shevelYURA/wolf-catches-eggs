0af3d180f8f3549c30fbabdafe05da2ba410e9d0
alina05dagestan <kseniaalieva171@gmail.com>
2026-04-17 15:52:57 +0300

    Update Eggs.h
    Новая картинка яйца, добавила путь к новой текстуре и методы

--- Eggs.h
+++ Eggs.h
@@ -1,20 +1,19 @@
 #pragma once
 #include "FallingObject.h"
-#include "resource.h"
 
 class Egg : public FallingObject {
 private:
-    bool isGolden;  // золотое яйцо или нет
+    bool isGolden; // Золотое яйцо или нет
 
 public:
-    Egg() : FallingObject(IDB_PNG5, Vector2f(40, 35)), isGolden(false) {}
+    Egg() : FallingObject("image/egg.png", Vector2f(40, 35)), isGolden(false) {}
     
     void setGolden(bool golden) {
         isGolden = golden;
         if (isGolden) {
-            setColor(sf::Color(255, 215, 0));  // Золотой цвет
+            setColor(sf::Color(255, 215, 0)); // Золотой цвет
         } else {
-            setColor(sf::Color::White);        // Белый цвет
+            setColor(sf::Color::White); // Белый цвет
         }
     }
     
