a46ff33868ea40c8984c58276247c50df0b7e136
shevelYURA <promacterto@gmail.com>
2026-05-13 00:01:52 +0300

    bugfix

--- ResourceManager.h
+++ ResourceManager.h
@@ -74,7 +74,7 @@ private:
             {IDB_PNG4, "boss.png"},
             {IDB_PNG5, "egg.png"},
             {IDB_PNG6, "icon.png"},
-            {IDB_POWERUP, "powerup.png"}
+            {IDB_POWERUP, "powerup.png"},
             {IDB_PNG6, "icon.png"},
             {IDB_BACKGROUND, "fon.jpg"}
         };
