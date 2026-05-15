7f11a7bb885d253177fb70019d0bdb9e54b75ee6
masha95chechnya <mariya.valanikova@gmail.com>
2026-05-15 20:31:21 +0300

    Update PowerUp.h

--- PowerUp.h
+++ PowerUp.h
@@ -5,7 +5,9 @@
 enum class PowerUpType {
     DoublePoints,
     EggRain,
-    BoxingGlove
+    BoxingGlove,
+    Burger,    // ← ВОССТАНАВЛИВАЕТ 25 HP (ЧАСТО)
+    Potion     // ← ВОССТАНАВЛИВАЕТ 40 HP (РЕДКО)
 };
 
 class PowerUp : public FallingObject {
@@ -15,7 +17,9 @@ private:
 public:
     PowerUp(PowerUpType t = PowerUpType::EggRain) 
         : FallingObject(
-            (t == PowerUpType::BoxingGlove) ? IDB_BOXING_GLOVE : IDB_POWERUP,
+            (t == PowerUpType::BoxingGlove) ? IDB_BOXING_GLOVE :
+            (t == PowerUpType::Burger) ? IDB_BURGER :
+            (t == PowerUpType::Potion) ? IDB_POTION : IDB_POWERUP,
             Vector2f(40, 40)
           ), type(t) {}
 
