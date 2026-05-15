36fafbedc30dcf0834fe4ce054cf71d4c86c80c8
masha95chechnya <mariya.valanikova@gmail.com>
2026-05-15 20:53:56 +0300

    Update resource.h

--- resource.h
+++ resource.h
@@ -4,22 +4,21 @@
 //
 #define IDB_PNG1                        103
 #define IDB_PNG2                        104
-#define IDB_PNG3                        105
 #define IDB_PNG4                        106
 #define IDB_PNG5                        107
 #define IDB_PNG6                        108
 #define IDB_POWERUP                     109
 #define IDB_BACKGROUND                  110
 #define IDB_BOXING_GLOVE                111
-#define IDB_BURGER                      112
-#define IDB_POTION                      113
 #define FONT_ARCADE                     201
-
+#define IDB_BOSS                        119
+#define IDB_BOMB                        113
+#define IDB_BASKET                      112
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        114
+#define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
